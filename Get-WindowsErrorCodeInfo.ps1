ac0699a9fa87e81a22a6cd36028002f109763bf7
Prateek Singh <Prateeksingh1590@gmail.com>
2016-06-14 16:19:23 +0530

    Update Get-WindowsErrorCodeInfo.ps1

--- Get-WindowsErrorCodeInfo.ps1
+++ Get-WindowsErrorCodeInfo.ps1
@@ -7,9 +7,7 @@ Param(
         [String]$GithubJSONUrl = "https://gist.githubusercontent.com/PrateekKumarSingh/72e5deda930831f0bb41a3518d2464e1/raw/ce98cc4120066b53ae738abf7d38668980cbaafd/WindowsErrorCode.JSON"
 )
 
-Begin{}
-
-Process
+Begin
 {
     Try
     {
@@ -17,34 +15,33 @@ Process
         If($LocalJSONFilePath)
         {
             $JSON = Get-Content .\ErrorCodes.json -Raw |ConvertFrom-Json
-			
-			If($ExitCode)
-			{
-				$JSON.PSObject.BaseObject | Where{$_.exitCode -eq $ExitCode}
-			}
-			Else
-			{
-				$JSON.PSObject.BaseObject 
-			}
         }
         ElseIf($GithubJSONUrl)
         {
             $JSON = (Invoke-WebRequest -Uri $GithubJSONUrl).content | ConvertFrom-Json
-			
-			If($ExitCode)
-			{
-				$JSON.PSObject.BaseObject | Where{$_.exitCode -eq $ExitCode}
-			}
-			Else
-			{
-				$JSON.PSObject.BaseObject
-			}
         }
     }
     Catch
     {
-        "Something went wrong! please try running the command again."
+        "Something went wrong! please try running the script again."
     }
+}
+
+Process
+{
+    Foreach($Item in $ExitCode)
+    {
+        If($Item)
+	    {
+			$JSON.PSObject.BaseObject | Where{$_.exitCode -eq $ExitCode}
+	    }
+        else
+        {
+            $JSON.PSObject.BaseObject 
+            break;
+        }
+    }
+
 }
 
 End
